e1bef686064b9475481a1fdb60526231ce12dc82
Marcelo Kopmann <16388408+Markkop@users.noreply.github.com>
2022-08-21 13:38:35 -0300

    Update stands.ts

--- src/data/stands.ts
+++ src/data/stands.ts
@@ -4,5 +4,89 @@ export const stands = [
     friendlyMapId: 'codecon-gather-api-test-1',
     topLeftPosition: { x: 3, y: 4 },
     bottomRightPosition: { x: 6, y: 7 }
-  }
+  },
+  {
+    name: 'Unico',
+    friendlyMapId: 'codecon-2022-sala-1',
+    topLeftPosition: { x: 10, y: 22 },
+    bottomRightPosition: { x: 21, y: 27 }
+  },
+  {
+    name: 'LinuxTips',
+    friendlyMapId: 'codecon-2022-sala-1',
+    topLeftPosition: { x: 39, y: 19 },
+    bottomRightPosition: { x: 44, y: 23 }
+  },
+  {
+    name: 'Tractian',
+    friendlyMapId: 'codecon-2022-sala-1',
+    topLeftPosition: { x: 50, y: 23 },
+    bottomRightPosition: { x: 54, y: 28 }
+  },
+  {
+    name: 'Trybe',
+    friendlyMapId: 'codecon-2022-sala-1',
+    topLeftPosition: { x: 7, y: 48 },
+    bottomRightPosition: { x: 18, y: 53 }
+  },
+  {
+    name: 'Trybe',
+    friendlyMapId: 'codecon-2022-sala-1',
+    topLeftPosition: { x: 7, y: 48 },
+    bottomRightPosition: { x: 18, y: 53 }
+  },
+  {
+    name: 'DevsCansados',
+    friendlyMapId: 'codecon-2022-sala-1',
+    topLeftPosition: { x: 39, y: 50 },
+    bottomRightPosition: { x: 44, y: 55 }
+  },
+  {
+    name: 'DVLPR',
+    friendlyMapId: 'codecon-2022-sala-2',
+    topLeftPosition: { x: 5, y: 45 },
+    bottomRightPosition: { x: 10, y: 48 }
+  },
+  {
+    name: 'GenioDesks',
+    friendlyMapId: 'codecon-2022-sala-2',
+    topLeftPosition: { x: 16, y: 42 },
+    bottomRightPosition: { x: 21, y: 46 }
+  },
+  {
+    name: 'Casa do Código',
+    friendlyMapId: 'codecon-2022-sala-2',
+    topLeftPosition: { x: 19, y: 51 },
+    bottomRightPosition: { x: 24, y: 54 }
+  },
+  {
+    name: 'Gather',
+    friendlyMapId: 'codecon-2022-sala-2',
+    topLeftPosition: { x: 51, y: 47 },
+    bottomRightPosition: { x: 55, y: 52 }
+  },
+  {
+    name: 'JetBrains',
+    friendlyMapId: 'codecon-2022-sala-2',
+    topLeftPosition: { x: 39, y: 18 },
+    bottomRightPosition: { x: 50, y: 23 }
+  },
+  {
+    name: 'seila',
+    friendlyMapId: 'codecon-2022-sala-2',
+    topLeftPosition: { x: 10, y: 23 },
+    bottomRightPosition: { x: 21, y: 27 }
+  },
+  {
+    name: 'Revelo',
+    friendlyMapId: 'codecon-2022-sala-3',
+    topLeftPosition: { x: 9, y: 22 },
+    bottomRightPosition: { x: 20, y: 27 }
+  },
+  {
+    name: 'GbTech',
+    friendlyMapId: 'codecon-2022-sala-3',
+    topLeftPosition: { x: 46, y: 23 },
+    bottomRightPosition: { x: 57, y: 28 }
+  },
 ]
