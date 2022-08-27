4062018692442c91c373f221a8717b1bc1e93db5
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-08-27 18:54:06 -0300

    feat: play sounds on interaction

--- src/events/onPlayerInteraction.ts
+++ src/events/onPlayerInteraction.ts
@@ -1,11 +1,20 @@
 import { Game } from "@gathertown/gather-game-client";
 import { ServerClientEventContext } from "@gathertown/gather-game-client/dist/src/public/utils";
+import { playSound } from "../interactions/playerInteracts/playSound";
 import { trackInteractions } from "../interactions/playerInteracts/trackInteractions"
 import { updateUserStatusWithRandomTitle } from "../interactions/playerInteracts/updateUserStatusWithRandomTitle";
 import { PlayerInteractsEventData } from "../types"
 
+function playSoundWithCategory(category: string) {
+  return (data: PlayerInteractsEventData, context: ServerClientEventContext, game: Game) => {
+    return playSound(data, context, game, category)
+  }
+}
+
 const actionsByObjectId: Record<string, Function> = {
-  'Balloons3 - r-SrSiX2I6ZgmaKLCLmSu_fd4a8132-1da5-456c-a297-7e785df0ee38': updateUserStatusWithRandomTitle
+  'Balloons3 - r-SrSiX2I6ZgmaKLCLmSu_fd4a8132-1da5-456c-a297-7e785df0ee38': updateUserStatusWithRandomTitle,
+  'Blank - QBiYhmko0be5JCAk5QsH_cea893db-ae23-4062-8a40-f57a33d4d7cd': playSoundWithCategory('ratinho'),
+  'Blank - QBiYhmko0be5JCAk5QsH_e7df7c9f-2abf-4ece-a84d-89655dd0a176': playSoundWithCategory('faro'),
 }
 
 export function onPlayerInteraction(data: PlayerInteractsEventData, context: ServerClientEventContext, game: Game) {
@@ -19,5 +28,5 @@ export function onPlayerInteraction(data: PlayerInteractsEventData, context: Ser
   const action = actionsByObjectId[interactedObjId]
   if (!action) return
 
-  action(data, context)
+  action(data, context, game)
 }
