9aaa60267c5e38a2982340b2f31e2c68c10010d8
Gabriel Nunes <gabriel.nunes@asaas.com.br>
2022-08-29 12:07:31 -0300

    feat: ajustar sons

--- src/interactions/playerInteracts/playSound.ts
+++ src/interactions/playerInteracts/playSound.ts
@@ -11,21 +11,21 @@ const sounds: Record<string, string[]> = {
     'https://www.myinstants.com/media/sounds/pare.mp3',
     'https://www.myinstants.com/media/sounds/comedy-male-yelling-yee-ha-sound-effects-free-download-mp3cut.mp3',
     'https://www.myinstants.com/media/sounds/ratinhooo_1.mp3',
-    'https://www.myinstants.com/media/sounds/uepa-ratinho-nho-nho_70k.mp3',
-    'https://www.myinstants.com/media/sounds/a-bobina-oooooo-bobina1.mp3'
+    'https://www.myinstants.com/media/sounds/uepa-mp3cut.mp3',
+    'https://www.myinstants.com/media/sounds/que-isso-meu-filho-calma_3cphAl0.mp3',
+    'https://www.myinstants.com/media/sounds/xaropinho-rapaaaaz.mp3',
   ],
   faro: [
     'https://www.myinstants.com/media/sounds/y2mate_rLgMJTu.mp3',
     'https://www.myinstants.com/media/sounds/cavaalo0.mp3',
     'https://www.myinstants.com/media/sounds/e-brincadeira-hein-rodrigo-faro.mp3',
     'https://www.myinstants.com/media/sounds/demais-faro-hq_WRnItjN.mp3',
+    'https://www.myinstants.com/media/sounds/ele-g0sta.mp3',
+    'https://www.myinstants.com/media/sounds/aiaiai-rodrigo-faro.mp3',
+    'https://www.myinstants.com/media/sounds/ch3ga.mp3',
+    'https://www.myinstants.com/media/sounds/tome-rodrigo-faro_xDXKGwq.mp3',
+    'https://www.myinstants.com/media/sounds/ui-rodrigo-faro.mp3'
   ],
-  alexa: [
-    'https://soundspunos.com/uploads/files/2022-01/1641632526_1641200600_alexa_reminder.mp3'
-  ],
-  fire: [
-    'https://soundspunos.com/uploads/files/2018-12/1544510643_fire_zkyuzme_.mp3'
-  ]
 }
 
 export async function playSound(data: PlayerInteractsEventData, context: ServerClientEventContext, game: Game, category: string) {
