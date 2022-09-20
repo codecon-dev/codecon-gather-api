dd52288ed148656086c75d634eeee0f9df5c59f3
Marcelo Kopmann <marcelokopmann@gmail.com>
2022-09-19 21:04:33 -0300

    feat: add bugs

--- src/data/bugs.ts
+++ src/data/bugs.ts
@@ -2,22 +2,127 @@ import { BugCollection } from "../types";
 
 export const bugCollections: BugCollection[] = [
   {
-    name: 'First',
+    name: "01",
     bugs: [
       {
-        objId: 'BarrelLitMetal - Nc5jVuh8lyMUbpZlCZVTa_29963bad-bab6-4749-b7cb-d004f8d554eb',
-        spaceId: 'VSqg1CcrGZHUwtaT/Codecon-2022',
-        mapId: 'escadas',
+        "objId": "yjnEwN1AWsjf0ZIgF03s_1815dbbd-3ea6-4919-b8b8-8391bd7025ec",
+        "spaceId": "VSqg1CcrGZHUwtaT/Codecon-2022",
+        "mapId": "qS4c3TKl_IR7fa01chmRL"
       },
       {
-        objId: 'BarrelLitMetal - Nc5jVuh8lyMUbpZlCZVTa_3308167f-ec69-4cd9-91b9-109e86b63d1f',
-        spaceId: 'VSqg1CcrGZHUwtaT/Codecon-2022',
-        mapId: 'escadas',
+        "objId": "cOjmryPf-m-BwgxbIYgG_25c7affb-11b4-45a4-988f-1845b2fa0b1d",
+        "spaceId": "VSqg1CcrGZHUwtaT/Codecon-2022",
+        "mapId": "escadas"
       },
       {
-        objId: 'BarrelLitMetal - Nc5jVuh8lyMUbpZlCZVTa_e8ab5d6d-94e9-4ff4-a490-e2ee1eaba428',
-        spaceId: 'VSqg1CcrGZHUwtaT/Codecon-2022',
-        mapId: 'onboarding',
+        "objId": "PM8YB05PY28uZWwm6JgCh_37a163c6-054e-4995-bdb5-add5934acb34",
+        "spaceId": "SSoqWgxuuoJPlXM7/codecon-2022-atividades",
+        "mapId": "NoeTPW4_NFHdXoOkjweXa"
+      },
+      {
+        "objId": "PM8YB05PY28uZWwm6JgCh_aab7e23b-30c3-47e2-9f22-55374782b3fb",
+        "spaceId": "SSoqWgxuuoJPlXM7/codecon-2022-atividades",
+        "mapId": "jSQVyzegXY0U9XBaxR--7"
+      },
+      {
+        "objId": "vTOHcWM4Uba3spuFWLV-b_b94aba5d-6a10-47d2-997c-375bf96d219e",
+        "spaceId": "DfVMRgRjpjJSoK5y/codecon-2022-sala-1",
+        "mapId": "Gzffv238NzvOqkbm9NaVy"
+      },
+      {
+        "objId": "RLS6Ac5UDJ0wTkXvthX0_4ef4f6a4-0607-4750-ba8a-16f564d9c4ec",
+        "spaceId": "aqQVlXTpX2ISp0ci/codecon-2022-sala-2",
+        "mapId": "sWap7b_BAuXHJIKzCoNgx"
+      },
+      {
+        "objId": "a30sTBCOk2ECSJlzUymF_4fa6dab6-b623-4bce-844e-468eff06b3de",
+        "spaceId": "ZXHxMgrgOseuus8N/codecon-2022-sala-3",
+        "mapId": "cV2hK1iJ2d_o0WVotlf20"
+      },
+      {
+        "objId": "ufJEiBGWUSeEGTKAUTdYZ_ebbaf671-6ff5-40d1-92ac-2e74f623a38d",
+        "spaceId": "ZXHxMgrgOseuus8N/codecon-2022-sala-3",
+        "mapId": "HMF3qeHREGqZYlBkwKaMy"
+      },
+    ]
+  },
+  {
+    name: "02",
+    bugs: [
+      {
+        "objId": "386-wnRwEykSwk9v2XlU_a49a12c0-e78f-40d2-a7dc-c10a2b2ce07f",
+        "spaceId": "VSqg1CcrGZHUwtaT/Codecon-2022",
+        "mapId": "b2h27gMYywH5rGahxRxl3"
+      },
+      {
+        "objId": "PM8YB05PY28uZWwm6JgCh_343eaeb5-b616-49db-9f27-875ab3018a6f",
+        "spaceId": "SSoqWgxuuoJPlXM7/codecon-2022-atividades",
+        "mapId": "NoeTPW4_NFHdXoOkjweXa"
+      },
+      {
+        "objId": "9PUhPOceIa2nq6CZIyeqn_2807612d-e1e9-42af-87e6-d0fff9513bcf",
+        "spaceId": "SSoqWgxuuoJPlXM7/codecon-2022-atividades",
+        "mapId": "jSQVyzegXY0U9XBaxR--7"
+      },
+      {
+        "objId": "vTOHcWM4Uba3spuFWLV-b_05cf72da-7a37-4fe7-beb5-c481319ee7ab",
+        "spaceId": "DfVMRgRjpjJSoK5y/codecon-2022-sala-1",
+        "mapId": "dwcUtMr8sOht3XI1VvbAe"
+      },
+      {
+        "objId": "8hcOfissEkud8ZOYn1hv_238e89f1-c574-4b70-9922-4529457d60a4",
+        "spaceId": "aqQVlXTpX2ISp0ci/codecon-2022-sala-2",
+        "mapId": "84znn8A-iPkAhC8T8dxeY"
+      },
+      {
+        "objId": "a30sTBCOk2ECSJlzUymF_4706bad8-cd14-4a87-90a9-a352be810ccd",
+        "spaceId": "ZXHxMgrgOseuus8N/codecon-2022-sala-3",
+        "mapId": "cV2hK1iJ2d_o0WVotlf20"
+      },
+      {
+        "objId": "a30sTBCOk2ECSJlzUymF_c4748099-15a0-4e4c-b94e-cb32d004c99f",
+        "spaceId": "ZXHxMgrgOseuus8N/codecon-2022-sala-3",
+        "mapId": "--jlyOAYhHt_Cd68kBiPO"
+      },
+    ]
+  },
+  {
+    name: "03",
+    bugs: [
+      {
+        "objId": "cOjmryPf-m-BwgxbIYgG_34611c48-1d7c-43ca-84e5-12a6631ca43a",
+        "spaceId": "VSqg1CcrGZHUwtaT/Codecon-2022",
+        "mapId": "b2h27gMYywH5rGahxRxl3"
+      },
+      {
+        "objId": "386-wnRwEykSwk9v2XlU_aca49f5d-06aa-4ba1-9797-c58e8c6c0afe",
+        "spaceId": "VSqg1CcrGZHUwtaT/Codecon-2022",
+        "mapId": "qS4c3TKl_IR7fa01chmRL"
+      },
+      {
+        "objId": "9PUhPOceIa2nq6CZIyeqn_1e045b85-2e85-410e-99fd-3f8fbf0a403f",
+        "spaceId": "SSoqWgxuuoJPlXM7/codecon-2022-atividades",
+        "mapId": "NoeTPW4_NFHdXoOkjweXa"
+      },
+      {
+        "objId": "PM8YB05PY28uZWwm6JgCh_1dcf5bbc-c07e-44e3-a392-d0454e98278f",
+        "spaceId": "SSoqWgxuuoJPlXM7/codecon-2022-atividades",
+        "mapId": "jSQVyzegXY0U9XBaxR--7"
+      },
+      {
+        "objId": "a9HQAG2tKy2c3sOtrGuBD_6ffc0992-175c-4108-9771-f82466290e18",
+        "spaceId": "DfVMRgRjpjJSoK5y/codecon-2022-sala-1",
+        "mapId": "Gzffv238NzvOqkbm9NaVy"
+      },
+      {
+        "objId": "ufJEiBGWUSeEGTKAUTdYZ_8cb83429-7918-4b7c-a6a3-ab7ff7c4b656",
+        "spaceId": "ZXHxMgrgOseuus8N/codecon-2022-sala-3",
+        "mapId": "cV2hK1iJ2d_o0WVotlf20"
+      },
+      {
+        "objId": "ufJEiBGWUSeEGTKAUTdYZ_0ebb9888-0b4f-4cb7-8202-86774b453b5a",
+        "spaceId": "ZXHxMgrgOseuus8N/codecon-2022-sala-3",
+        "mapId": "--jlyOAYhHt_Cd68kBiPO"
       }
     ]
   }
